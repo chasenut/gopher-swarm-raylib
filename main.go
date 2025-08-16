144056d93b412ab065343ce0bbfae54ae026b6a0
Cashtann <switalamaciej5@gmail.com>
2025-08-16 11:59:03 +0200

    added moving camera

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 )
 
 const (
-	windowWidth int32 = 1200
-	windowHeight int32 = 900
+	screenWidth int32 = 1200
+	screenHeight int32 = 900
 	FPS int32 = 60
 )
 
@@ -22,40 +22,93 @@ var (
 	
 	perlinImg *rl.Image 
 	perlinTexture rl.Texture2D
+
+	cameraTarget rl.Vector2
+	cameraOffset rl.Vector2
+	camera rl.Camera2D
+	cameraSpeed float32 = 250
+	cameraMoving bool = false
+	cameraUp, cameraDown, cameraRight, cameraLeft = false, false, false, false
 )
 
 func input() {
-
+	if rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyUp) {
+		cameraMoving = true
+		cameraUp = true
+	}
+	if rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyDown) {
+		cameraMoving = true
+		cameraDown = true
+	}
+	if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight) {
+		cameraMoving = true
+		cameraRight = true
+	}
+	if rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyLeft) {
+		cameraMoving = true
+		cameraLeft = true
+	}
+	if rl.IsKeyPressed(rl.KeyQ) {
+	}
 }
 
 func update() {
 	running = !rl.WindowShouldClose()
 	dt = rl.GetFrameTime()
 
-	hueTint = rl.ColorFromHSV(float32(rl.GetTime() * 10000.0 / 360.0), 1, 1)
+	hueTint = rl.ColorFromHSV(float32(rl.GetTime() * 1000 / 36), 1, 1)
+
+	if cameraMoving {
+		if cameraUp {
+			cameraOffset.Y -= cameraSpeed * dt
+		}
+		if cameraDown {
+			cameraOffset.Y += cameraSpeed * dt
+		}
+		if cameraRight {
+			cameraOffset.X += cameraSpeed * dt
+		}
+		if cameraLeft {
+			cameraOffset.X -= cameraSpeed * dt
+		}
+	}
+	cameraMoving = false
+	cameraUp, cameraDown, cameraRight, cameraLeft = false, false, false, false
+	camera.Target = cameraOffset
 }
 
 func drawScene() {
-	rl.BeginDrawing()
-
 	rl.ClearBackground(bkgColor)
 
 
 	rl.DrawTexture(perlinTexture, 0, 0, hueTint)
 
-	rl.DrawText(fmt.Sprintf("%v", rl.GetFPS()), 0, 0, 40, rl.LightGray)
-	rl.EndDrawing()
-
-
+	rl.DrawText(fmt.Sprintf("%.2f", float32(1.0 / dt)),
+		int32(cameraOffset.X)-screenWidth/2+20, 
+		int32(cameraOffset.Y)-screenHeight/2+20, 
+		40, rl.LightGray)
 }
 
 func render() {
-	
+	rl.BeginDrawing()
+	rl.BeginMode2D(camera)
+
+	drawScene()
+
+	rl.EndMode2D()
+	rl.EndDrawing()
 }
 
 func initialize() {
-	rl.InitWindow(windowWidth, windowHeight, "hello raylib")
+	rl.InitWindow(screenWidth, screenHeight, "hello raylib")
 	rl.SetTargetFPS(FPS)
+	rl.SetExitKey(0)
+
+	cameraTarget.X = float32(screenWidth/2)
+	cameraTarget.Y = float32(screenHeight/2)
+	cameraOffset.X = 0
+	cameraOffset.Y = 0
+	camera = rl.NewCamera2D(cameraTarget, cameraOffset, 1, 1)
 
 	perlinImg = rl.GenImagePerlinNoise(800, 800, 0, 0, 5)
 	perlinTexture = rl.LoadTextureFromImage(perlinImg)
@@ -75,6 +128,7 @@ func main() {
 	for running {
 		input()
 		update()
-		drawScene()
+		render()
 	}
+	exit()
 }
